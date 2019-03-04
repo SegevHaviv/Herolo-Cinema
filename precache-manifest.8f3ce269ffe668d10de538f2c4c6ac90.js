a09c01fcb4b764f04ac2e4b6eba1f7ac4bc74b15
segevhaviv <segevhaviv@gmail.com>
2019-03-05 00:18:28 +0200

    Updates

---
+++ precache-manifest.8f3ce269ffe668d10de538f2c4c6ac90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40e8d397db81f59fe517",
+    "url": "/Herolo-Cinema/static/css/main.958ea6e8.chunk.css"
+  },
+  {
+    "revision": "40e8d397db81f59fe517",
+    "url": "/Herolo-Cinema/static/js/main.40e8d397.chunk.js"
+  },
+  {
+    "revision": "38d059cec60206ca46b9",
+    "url": "/Herolo-Cinema/static/js/runtime~main.38d059ce.js"
+  },
+  {
+    "revision": "2837cf666c66d81ff6de",
+    "url": "/Herolo-Cinema/static/js/2.2837cf66.chunk.js"
+  },
+  {
+    "revision": "6e1a15dcb1b97b0aeb121da5a09e3154",
+    "url": "/Herolo-Cinema/static/media/bg.6e1a15dc.jpg"
+  },
+  {
+    "revision": "d58e187c82a39dd636aa8d5560fab462",
+    "url": "/Herolo-Cinema/index.html"
+  }
+];
